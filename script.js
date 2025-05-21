f2c896e8a57f4646cf2d229c5e6ff2e35f2d358c
chintoooo <jeevansatishbonthu123@gmail.com>
2025-05-21 18:08:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, index) => {
+  input.addEventListener('input', () => {
+    const value = input.value;
+    if (value) {
+      input.value = value.slice(0, 1);
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    }
+  });
+
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (input.value === '') {
+        if (index > 0) {
+          inputs[index - 1].focus();
+          inputs[index - 1].value = '';
+        }
+      } else {
+        input.value = '';
+      }
+      e.preventDefault();
+    }
+  });
+
+  input.addEventListener('paste', (e) => {
+    e.preventDefault();
+    const pasteData = e.clipboardData.getData('text').replace(/\D/g, '');
+    pasteData.split('').forEach((char, i) => {
+      if (i < inputs.length) {
+        inputs[i].value = char;
+      }
+    });
+    const lastIndex = Math.min(pasteData.length, inputs.length) - 1;
+    if (lastIndex >= 0) inputs[lastIndex].focus();
+  });
+});
+
+window.addEventListener('load', () => {
+  inputs[0].focus();
+});
